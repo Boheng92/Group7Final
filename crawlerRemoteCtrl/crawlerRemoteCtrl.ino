952799b4e7a12f2511241da0a177c68f74ba37f3
zhuyidsg <zhuyidsg@bu.edu>
2015-12-01 15:15:15 -0500

    First Commit
    Add template

---
+++ crawlerRemoteCtrl/crawlerRemoteCtrl.ino
@@ -0,0 +1,130 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+#include "stdlib.h"
+ 
+Servo wheels; // servo for turning the wheels
+Servo esc; // not actually a servo, but controlled like one!
+bool startup = true; // used to ensure startup only happens once
+int startupDelay = 1000; // time to pause at each calibration step
+double maxSpeedOffset = 45; // maximum speed magnitude, in servo 'degrees'
+double maxWheelOffset = 85; // maximum wheel turn magnitude, in servo 'degrees'
+double wheelOffset = 0.0; // For Adjusting the wheel
+
+SoftwareSerial XBee(2, 3);
+ 
+void setup()
+{
+  wheels.attach(8); // initialize wheel servo to Digital IO Pin #8
+  esc.attach(9); // initialize ESC to Digital IO Pin #9
+  /*  If you're re-uploading code via USB while leaving the ESC powered on, 
+   *  you don't need to re-calibrate each time, and you can comment this part out.
+   */
+   Serial.begin(9600);
+
+   XBee.begin(9600);
+   //Serial.begin(9600);
+   
+   calibrateESC();
+}
+
+/* Convert degree value to radians */
+double degToRad(double degrees){
+  return (degrees * 71) / 4068;
+}
+
+/* Convert radian value to degrees */
+double radToDeg(double radians){
+  return (radians * 4068) / 71;
+}
+
+/* Calibrate the ESC by sending a high signal, then a low, then middle.*/
+void calibrateESC(){
+    esc.write(180); // full backwards
+    delay(startupDelay);
+    esc.write(0); // full forwards
+    delay(startupDelay);
+    esc.write(90); // neutral
+    delay(startupDelay);
+    esc.write(90); // reset the ESC to neutral (non-moving) value
+}
+
+/* Oscillate between various servo/ESC states, using a sine wave to gradually 
+ *  change speed and turn values.
+ */
+void oscillate(){
+  for (int i =0; i < 360; i++){
+    double rad = degToRad(i);
+    double speedOffset = sin(rad) * maxSpeedOffset;
+    double wheelOffset = sin(rad) * maxWheelOffset;
+//    Serial.println(90 + speedOffset);
+    Serial.println(90 + wheelOffset);
+//    esc.write(90 + speedOffset);
+//    wheels.write(90 + wheelOffset);
+    delay(50);
+  }
+}
+
+void steerRight(double d)
+{
+//  Serial.write("Steer Right:");
+//  Serial.write("\n");
+  
+  if( (d >= 0.0 ) && (d <= 1.0))
+  {
+    double temp = min( (d * maxWheelOffset + wheelOffset), maxWheelOffset);
+//    Serial.println("temp :  "+ (String)temp);
+    
+    wheels.write(90 - temp);
+  }
+}
+
+void steerLeft(double d)
+{ 
+//  Serial.write("Steer Left:");
+//  Serial.write("\n");
+  
+  if( (d >= 0.0 ) && (d <= 1.0))
+  {
+    double temp = min( (d * maxWheelOffset + wheelOffset), maxWheelOffset);
+    
+    wheels.write(90 + temp);
+  }
+}
+
+void setVelocity(double s)
+{
+  if( (s >= -1.0 ) && (s <= 1.0))
+  {
+    esc.write(90 - (s * maxSpeedOffset));
+  }
+}
+ 
+void loop()
+{
+  delay(500);
+  
+  char c = XBee.read();
+
+  if(c == 'F')
+  {
+    // Foward
+    setVelocity(0.3);
+  }
+  else if(c == 'B')
+  {
+    // Backward
+    setVelocity(-0.3);
+  }
+  else if(c == 'L')
+  {
+    // Left
+    steerLeft(0.8);
+  }
+  else if(c == 'R')
+  {
+    // Right
+    steerRight(0.8);      
+  }
+}
+
+
